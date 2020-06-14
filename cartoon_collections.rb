65c05722752c9b66a3fca932c0ea6a6c8c8b14a0
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-14 22:17:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   new_array = []
-  array.each do |element|
+  array.map do |element|
     new_array << element * element
   end
   new_array
@@ -11,6 +11,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+
 end
 
 def long_planeteer_calls(planeteer_calls)
